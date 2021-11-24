e6f0e625f3f2b1cfcec9de4bc2c12e9d72f35061
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2021-11-24 10:05:14 +0330

    Add project files.

---
+++ Demo1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Demo1.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+          
+            return View();
+        }
+
+        public IActionResult SetSession(string name)
+        {
+            HttpContext.Session.SetObject<string>("k1", "test");
+            
+            return RedirectToAction("index");
+        }
+
+      
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    
+    }
+}
