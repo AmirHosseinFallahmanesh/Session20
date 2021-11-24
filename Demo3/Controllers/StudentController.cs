ba4b25e2b2797e7bec1f5040fd0eae358ef9c3ad
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2021-11-24 12:25:57 +0330

    api

---
+++ Demo3/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    { 
+        
+        public IStudentService StudentService { get; }
+        public StudentController(IStudentService studentService)
+        {
+            StudentService = studentService;
+        }
+
+
+        [HttpGet]
+        public List<Student> GetAll()
+        {
+            var res= StudentService.GetAll();
+            return res;
+        }
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return StudentService.Get(id);
+        }
+
+
+        [HttpPost]
+        public void Post( Student student)
+        {
+            StudentService.Add(student);
+        }
+        [HttpPut]
+        public void Put(Student student)
+        {
+            StudentService.Update(student);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Student student = StudentService.Get(id);
+
+            StudentService.Delete(student);
+        }
+
+    }
+}
---
+++ Demo3/Model/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo3.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+    public interface IStudentService
+    {
+        List<Student> GetAll();
+        Student Get(int id);
+
+        void Add(Student student);
+        void Update(Student student);
+
+        void Delete(Student student);
+    }
+    public class StudentService : IStudentService
+    {
+        List<Student> students;
+        public StudentService()
+        {
+            students = new List<Student>()
+            {
+                new Student(){Id=1,Name="amir",Surname="amiri"},
+                new Student(){Id=2,Name="reza",Surname="rezaii"}
+            };
+        }
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void Delete(Student student)
+        {
+            students.Remove(student);
+        }
+
+        public Student Get(int id)
+        {
+            return students.Single(a => a.Id == id);
+        }
+
+        public List<Student> GetAll()
+        {
+            return students;
+        }
+
+        public void Update(Student student)
+        {
+            int index = students.FindIndex(a => a.Id == student.Id);
+            students[index] = student;
+        }
+    }
+}
